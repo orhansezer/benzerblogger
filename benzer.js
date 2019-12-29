4ba3d8ca048cca83f78f76df66754bfb03cb031f
orhansezer <orhansezer@live.com>
2019-12-29 22:26:29 +0300

    Update benzer.js

--- benzer.js
+++ benzer.js
@@ -1,5 +1,104 @@
 /*!
- * Blogger Related Posts Widget
+ * Blogger Related Posts Widget v1.0.4
+ * https://github.com/salmanarshad2000/blogger-related-posts-widget
+ * Copyright (c) 2012-2015 Salman Arshad
  * Released under the MIT license
  */
-(function(){if(document.querySelector===undefined){return}var e={},b,a=[],f,d,c;e.maxPostsToFetch=(typeof bloggerRelatedPosts_config==="object"&&bloggerRelatedPosts_config.maxPostsToFetch)||100;e.maxPostsToDisplay=(typeof bloggerRelatedPosts_config==="object"&&bloggerRelatedPosts_config.maxPostsToDisplay)||5;b=document.querySelector("link[rel=canonical]").href;if(/\x2F\d{4}\x2F\d{2}\x2F/.test(b)===false){return}for(f=0,d=document.querySelectorAll("a[rel=tag]");f<d.length;f++){a.push(decodeURIComponent(d[f].href.split("/").pop()))}bloggerRelatedPosts_callback=function(o){var h=[],p,n,l,r,y,x,q,t,g,s,w,u,v,m;for(p=0,r=o.feed.entry;p<r.length;p++){y={title:r[p].title.$t,updated:new Date(r[p].updated.$t),categories:[],count:0};for(n=0,x=r[p].link;n<x.length;n++){if(x[n].rel==="alternate"){y.link=x[n].href;break}}if(y.link===b){continue}for(n=0,q=r[p].category;n<q.length;n++){y.categories.push(q[n].term);for(l=0;l<a.length;l++){if(a[l]===q[n].term){y.count++;break}}}if(r[p].media$thumbnail){y.icon={src:r[p].media$thumbnail.url,width:r[p].media$thumbnail.width,height:r[p].media$thumbnail.height}}h.push(y)}h.sort(function(j,i){return(i.count-j.count)||(i.updated-j.updated)});h=h.slice(0,e.maxPostsToDisplay);t=function(){if(typeof ga==="function"){var i=this;ga("send",{hitType:"event",eventCategory:"Blogger Related Posts",eventAction:"Related Post Clicked",eventLabel:i.href,hitCallback:function(){location.href=i.href}});return false}};g=document.createElement("div");g.id="blogger-related-posts";g.innerHTML="<h4>Benzer Yazılar</h4>";s=document.createElement("ul");for(p=0;p<h.length;p++){w=document.createElement("li");u=document.createElement("a");u.href=h[p].link;u.title=h[p].count+" common "+(h[p].count===1?"category":"categories");u.onclick=t;v=document.createElement("span");if(h[p].icon){v.setAttribute("style","background: url("+h[p].icon.src+") no-repeat center center;")}u.appendChild(v);u.appendChild(document.createTextNode(h[p].title));m=document.createElement("small");m.appendChild(document.createTextNode(h[p].categories.join(", ")));w.appendChild(u);w.appendChild(m);s.appendChild(w)}g.appendChild(s);document.querySelector(".post").appendChild(g)};c=document.createElement("script");c.src="/feeds/posts/summary?alt=json&callback=bloggerRelatedPosts_callback&max-results="+e.maxPostsToFetch+"&q="+encodeURIComponent('label:"'+a.join('" | label:"')+'"');document.querySelector("head").appendChild(c)})();
+(function() {
+	if (document.querySelector === undefined) {
+		return;
+	}
+	var config = {}, postLink, postCategories = [], i, links, script;
+	config.maxPostsToFetch = (typeof bloggerRelatedPosts_config === "object" && bloggerRelatedPosts_config.maxPostsToFetch) || 100;
+	config.maxPostsToDisplay = (typeof bloggerRelatedPosts_config === "object" && bloggerRelatedPosts_config.maxPostsToDisplay) || 5;
+	postLink = document.querySelector("link[rel=canonical]").href;
+	if (/\x2F\d{4}\x2F\d{2}\x2F/.test(postLink) === false) {
+		return;
+	}
+	for (i = 0, links = document.querySelectorAll("a[rel=tag]"); i < links.length; i++) {
+		postCategories.push(decodeURIComponent(links[i].href.split("/").pop()));
+	}
+	bloggerRelatedPosts_callback = function(data) {
+		var relatedPosts = [], i, j, k, entries, item, links, categories, clickHandler, div, ul, li, a, span, small;
+		for (i = 0, entries = data.feed.entry; i < entries.length; i++) {
+			item = {
+				title: entries[i].title.$t,
+				updated: new Date(entries[i].updated.$t),
+				categories: [],
+				count: 0
+			};
+			for (j = 0, links = entries[i].link; j < links.length; j++) {
+				if (links[j].rel === "alternate") {
+					item.link = links[j].href;
+					break;
+				}
+			}
+			if (item.link === postLink) {
+				continue;
+			}
+			for (j = 0, categories = entries[i].category; j < categories.length; j++) {
+				item.categories.push(categories[j].term);
+				for (k = 0; k < postCategories.length; k++) {
+					if (postCategories[k] === categories[j].term) {
+						item.count++;
+						break;
+					}
+				}
+			}
+			if (entries[i].media$thumbnail) {
+				item.icon = {
+					src: entries[i].media$thumbnail.url,
+					width: entries[i].media$thumbnail.width,
+					height: entries[i].media$thumbnail.height
+				};
+			}
+			relatedPosts.push(item);
+		}
+		relatedPosts.sort(function(item1, item2) {
+			return (item2.count - item1.count) || (item2.updated - item1.updated);
+		});
+		relatedPosts = relatedPosts.slice(0, config.maxPostsToDisplay);
+		clickHandler = function() {
+			if (typeof ga === "function") {
+				var link = this;
+				ga("send", {
+					hitType: "event",
+					eventCategory: "Blogger Related Posts",
+					eventAction: "Related Post Clicked",
+					eventLabel: link.href,
+					hitCallback: function() {
+						location.href = link.href;
+					}
+				});
+				return false;
+			}
+		};
+		div = document.createElement("div");
+		div.id = "blogger-related-posts";
+		div.innerHTML = "<h4>Benzer Yazılar</h4>";
+		ul = document.createElement("ul");
+		for (i = 0; i < relatedPosts.length; i++) {
+			li = document.createElement("li");
+			a = document.createElement("a");
+			a.href = relatedPosts[i].link;
+			a.title = relatedPosts[i].count + " common " + (relatedPosts[i].count === 1 ? "category" : "categories");
+			a.onclick = clickHandler;
+			span = document.createElement("span");
+			if (relatedPosts[i].icon) {
+				span.setAttribute("style", "background: url(" + relatedPosts[i].icon.src + ") no-repeat center center;");
+			}
+			a.appendChild(span);
+			a.appendChild(document.createTextNode(relatedPosts[i].title));
+			small = document.createElement("small");
+			small.appendChild(document.createTextNode(relatedPosts[i].categories.join(", ")));
+			li.appendChild(a);
+			li.appendChild(small);
+			ul.appendChild(li);
+		}
+		div.appendChild(ul);
+		document.querySelector(".post").appendChild(div);
+	};
+	script = document.createElement("script");
+	script.src = "/feeds/posts/summary?alt=json&callback=bloggerRelatedPosts_callback&max-results=" + config.maxPostsToFetch + "&q=" + encodeURIComponent('label:"' + postCategories.join('" | label:"') + '"');
+	document.querySelector("head").appendChild(script);
+})();
